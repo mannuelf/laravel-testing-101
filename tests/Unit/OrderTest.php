b85b4d4ee9ccfc6e58f271daf48a5d4c2431882b
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-01-13 12:47:05 +0200

    created order test class

---
+++ tests/Unit/OrderTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mannuelferreira
+ * Date: 2018/01/13
+ * Time: 12:35
+ */
+
+namespace Tests\Unit;
+
+use Order;
+use PHPUnit\Framework\TestCase;
+
+class OrderTest extends TestCase
+{
+
+}
