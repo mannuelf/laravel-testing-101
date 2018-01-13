5337c1dbaf34d41f74061a55815876364e3ebc9d
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-01-13 11:33:49 +0200

    added new functions to product class and created a setup function for a product instance

--- app/Product.php
+++ app/Product.php
@@ -5,5 +5,22 @@ namespace App;
 
 class Product
 {
+    protected $name;
+    protected $cost;
 
+    public function __construct($name, $cost)
+    {
+        $this->name = $name;
+        $this->cost = $cost;
+    }
+
+    public function name()
+    {
+        return $this->name;
+    }
+
+    public function cost()
+    {
+        return $this->cost;
+    }
 }
