bc3ca34741e2a3546f3addefc50a7a7a4d14084f
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-01-13 13:31:16 +0200

    refactor to use array_reduce

--- app/Order.php
+++ app/Order.php
@@ -20,13 +20,18 @@ class Order
     public function total()
     {
         // implementation
-        $total = 0;
+//        $total = 0;
 
         // filter through all the products call a cost method on each on and store the result.
-        foreach ($this->products as $product) {
-            $total += $product->cost();
-        }
+//        foreach ($this->products as $product) {
+//            $total += $product->cost();
+//        }
+//
+//        return $total;
 
-        return $total;
+        // refactor, possibly
+        return array_reduce($this->products, function($carry, $product) {
+           return $carry + $product->cost();
+        });
     }
 }
