a5c328ee43b403dd7e9b0b831d16747894ce7bd0
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-05-01 18:33:56 +0200

    likes test

---
+++ tests/Integration/LikesTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use Tests\TestCase;
+
+class LikesTest extends TestCase {
+
+    /** @test **/
+    public function a_user_can_like_a_post()
+    {
+        // given I have a post
+
+        // and a user
+        // and that a user is logged in
+
+        // when they like a post
+
+
+        // then we should see evidence in the database that a post is liked.
+
+    }
+}
